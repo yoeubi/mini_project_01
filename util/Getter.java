5f2df56579cd1e9cdb6a42b011c98f87caf97daf
DESKTOP-V1D2DGE\Jiyeon <jiyeonyoomoon@gmail.com>
2018-03-15 11:49:42 +0900

    utill

---
+++ util/Getter.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.util.Scanner;
+
+import org.apache.ibatis.session.SqlSession;
+
+import common.db.MyAppSqlConfig;
+import domain.BitMessenger;
+import main.ui.LoginUI;
+import mapper.BitMessengerMapper;
+
+public class Getter {
+	private static BitMessengerMapper mapper;
+	private static Scanner sc = new Scanner(System.in);
+	
+	
+	static {
+		SqlSession session = MyAppSqlConfig.getSqlSession();
+		mapper = session.getMapper(BitMessengerMapper.class);
+	}
+	
+	public static BitMessengerMapper getMapper() {
+		return mapper;
+	}
+	
+	
+	public static String getStr(String str) {
+		System.out.print(str);
+		return sc.nextLine();
+	}
+	
+	public static int getInt(String str) {
+		System.out.print(str);
+		return Integer.parseInt(sc.nextLine());
+	}
+	
+	// 로그인된  user information -jy
+	public static BitMessenger Loginuserinfo(BitMessenger info) {
+		return info;
+		
+	}
+	
+	
+}
